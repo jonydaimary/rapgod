7caa532db01ab9b3df9b7bfe8f85faceee76351f
badsahah <45558399+Ajitdaimary@users.noreply.github.com>
2018-12-22 15:12:41 +0530

    Update master.py

--- master.py
+++ master.py
@@ -1,14 +1,61 @@
-(@client.command(pass_context=True)  
-@commands.has_permissions(kick_members=True)     
-async def kick(ctx,user:discord.Member):
-    if user.server_permissions.kick_members:
-      await client.say('**He is mod/admin and i am unable to kick him/her**')
+import discord
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import asyncio
+import colorsys
+import random
+import platform
+from discord import Game, Embed, Color, Status, ChannelType
+import os
+import functools
+import time
+import datetime
+import requests
+import json
+import aiohttp
+
+
+Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
+client = commands.Bot(description="masterjoker Official Bot", command_prefix=commands.when_mentioned_or("mk!"), pm_help = True)
+client.remove_command('help')
+
+
+
+@client.event
+async def on_ready():
+	print('Logged in as '+client.user.name+'')
+	print('--------')
+	print('--------')
+	print('Started Meme king') #add_your_bot_name_here
+	return await client.change_presence(music=discord.Game(game='Master play bots')) #add_your_bot_status_here
+
+
+
+
+@client.command(pass_context = True)
+@commands.has_permissions(administrator=True) 
+async def announce(ctx, channel: discord.Channel=None, *, msg: str):
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed=discord.Embed(title="Announcement", description="{}".format(msg), color = discord.Color((r << 16) + (g << 8) + b))
+    await client.send_message(channel, embed=embed)
+    await client.delete_message(ctx.message)
+
+    
+    
+@client.command(pass_context=True)
+@commands.has_permissions(administrator=True)
+async def embed(ctx, *args):
+    if ctx.message.author.bot:
       return
     else:
-      await client.kick(user)
-      await client.say(user.name+' was kicked. Good bye '+user.name+'!')
-      await client.delete_message(ctx.message)
-      for channel in user.server.channels:
-        if channel.name == 'soyal-log':
-            embed=discord.Embed(title="User kicked!", description="**{0}** is kicked by **{1}**!".format(user, ctx.message.author), color=0xFDE112)
-            await client.send_message(channel, embed=embed)
+      argstr = " ".join(args)
+      r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+      text = argstr
+      color = discord.Color((r << 16) + (g << 8) + b)
+      await client.send_message(ctx.message.channel, embed=Embed(color = color, description=text))
+      await client.delete_message(ctx.message)    
+
+        
+        
+
+client.run(os.getenv('Token')) 
